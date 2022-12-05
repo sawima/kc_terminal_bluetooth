157de1cb0192709beb96f679616ba48058592365
James <james@kimatech.com>
2022-12-05 15:53:15 +0800

    kimacloud ble service

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"os/exec"
 	"time"
 
 	"tinygo.org/x/bluetooth"
@@ -48,16 +49,15 @@ type WifiSettingStatus struct {
 }
 
 var (
-	serviceUUID, _ = bluetooth.ParseUUID("d6cb1959-8010-43bd-8ef7-48dbd249b984")
-	refreshUUID, _ = bluetooth.ParseUUID("c537baa5-6201-4275-ab14-da353bde3dc3")
-	statusUUID, _  = bluetooth.ParseUUID("f9e9e098-77d4-4db3-a08f-8321c493431b")
-	ipUUID, _      = bluetooth.ParseUUID("2d75504c-b822-44b3-bb81-65d7b6cbdae1")
-	// readUUID, _    = bluetooth.ParseUUID("2d75504c-b822-44b3-bb81-65d7b6cbdae3")
-	settingUUID, _ = bluetooth.ParseUUID("493ebfb0-b690-4ae8-a77a-329619c6f613")
+	serviceUUID, _       = bluetooth.ParseUUID("d6cb1959-8010-43bd-8ef7-48dbd249b984")
+	refreshUUID, _       = bluetooth.ParseUUID("c537baa5-6201-4275-ab14-da353bde3dc3")
+	statusUUID, _        = bluetooth.ParseUUID("f9e9e098-77d4-4db3-a08f-8321c493431b")
+	ipUUID, _            = bluetooth.ParseUUID("2d75504c-b822-44b3-bb81-65d7b6cbdae1")
+	settingUUID, _       = bluetooth.ParseUUID("493ebfb0-b690-4ae8-a77a-329619c6f613")
+	resetTerminalUUID, _ = bluetooth.ParseUUID("2d75504c-b822-44b3-bb81-65d7b6cbdae3")
 )
 
 var localAPIHost = "http://127.0.0.1:3002/"
-
 var getLocalIPAddressURL = localAPIHost + "getLocalIPAddress"
 var internetHealthyCheckURL = localAPIHost + "internetHealthyCheck"
 var ideviceIsInitializedURL = localAPIHost + "deviceIsInitialized"
@@ -84,7 +84,7 @@ func main() {
 	var refreshChar bluetooth.Characteristic
 	var statusChar bluetooth.Characteristic
 	var ipChar bluetooth.Characteristic
-	// var readChar bluetooth.Characteristic
+	var resetTerminalChar bluetooth.Characteristic
 	var settingChar bluetooth.Characteristic
 	must("add service", adapter.AddService(&bluetooth.Service{
 		UUID: serviceUUID,
@@ -96,11 +96,9 @@ func main() {
 				WriteEvent: func(client bluetooth.Connection, offset int, value []byte) {
 					//todo: get network status
 					//todo: get ip addresses
-
 					ipaddresses, _ := getLocalIPAddresses()
 					ipString, _ := json.Marshal(ipaddresses)
 					ipChar.Write(ipString)
-
 					netState, _ := internetHealthyCheck()
 					if netState {
 						statusChar.Write([]byte("online"))
@@ -115,8 +113,6 @@ func main() {
 				UUID:   settingUUID,
 				Flags:  bluetooth.CharacteristicWritePermission | bluetooth.CharacteristicWriteWithoutResponsePermission,
 				WriteEvent: func(client bluetooth.Connection, offset int, value []byte) {
-					log.Println(value)
-					log.Println("write setting to ble")
 					setupNewWifi(value)
 					ipaddresses, _ := getLocalIPAddresses()
 					ipString, _ := json.Marshal(ipaddresses)
@@ -130,19 +126,15 @@ func main() {
 
 				},
 			},
-			// {
-			// 	Handle: &readChar,
-			// 	UUID:   readUUID,
-			// 	Flags:  bluetooth.CharacteristicWritePermission | bluetooth.CharacteristicWriteWithoutResponsePermission,
-			// 	WriteEvent: func(client bluetooth.Connection, offset int, value []byte) {
-			// 		// txChar.Write(value)
-
-			// 		ipaddresses, _ := getLocalIPAddresses()
-			// 		ipString, _ := json.Marshal(ipaddresses)
-			// 		log.Println(ipString)
-			// 		ipChar.Write(ipString)
-			// 	},
-			// },
+			{
+				Handle: &resetTerminalChar,
+				UUID:   resetTerminalUUID,
+				Flags:  bluetooth.CharacteristicWritePermission | bluetooth.CharacteristicWriteWithoutResponsePermission,
+				WriteEvent: func(client bluetooth.Connection, offset int, value []byte) {
+					log.Println("reset terminal")
+					resetTerminal()
+				},
+			},
 			{
 				Handle: &statusChar,
 				UUID:   statusUUID,
@@ -157,15 +149,14 @@ func main() {
 	}))
 
 	println("advertising...")
-
 	ipaddresses, _ := getLocalIPAddresses()
 	ipString, _ := json.Marshal(ipaddresses)
 	log.Println(ipString)
 	ipChar.Write(ipString)
 	address, _ := adapter.Address()
 	for {
-		println("Go Bluetooth test /", address.MAC.String())
-		time.Sleep(10 * time.Second)
+		println("Kimacloud Bluetooth Service /", address.MAC.String())
+		time.Sleep(500 * time.Microsecond)
 	}
 }
 
@@ -197,8 +188,6 @@ func getLocalIPAddresses() (DeviceIPAddress, error) {
 	defer res.Body.Close()
 	rbody, _ := ioutil.ReadAll(res.Body)
 	ipaddresses := DeviceIPAddress{}
-	log.Println("--")
-	log.Println(rbody)
 
 	err = json.Unmarshal(rbody, &ipaddresses)
 	if err != nil {
@@ -246,9 +235,6 @@ func getBleServiceName() (string, error) {
 }
 
 func setupNewWifi(wifiConfig []byte) (bool, error) {
-	// postData, _ := json.Marshal(wifiConfig)
-	log.Println("api to setup wifi")
-
 	request, _ := http.NewRequest("POST", setupNewWifiURL, bytes.NewBuffer(wifiConfig))
 	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
 
@@ -271,4 +257,15 @@ func setupNewWifi(wifiConfig []byte) (bool, error) {
 	}
 	log.Println(wifiSettingStatus)
 	return wifiSettingStatus.Success, nil
+}
+
+func resetTerminal() (bool, error) {
+	// startCMD, _ := exec.Command("echo", "start work on ").Output()
+	// log.Println(string(startCMD))
+	resetCMD := exec.Command("/home/player/resetTerminal")
+	var out bytes.Buffer
+	resetCMD.Stdout = &out
+	resetCMD.Run()
+	log.Println(out.String())
+	return true, nil
 }
